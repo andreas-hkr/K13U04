536cf18c4d1977b9521979b097563f2462117fa4
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 12:50:40 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     static RatNum simplify(RatNum num) {
-        // ...
+        int m = num.p;
+        int n = num.q;
 
         // Från 6.2
         while (true) {
@@ -12,7 +13,11 @@ public class Main {
             n = r;
         }
 
-        // ...
+        RatNum res = new RatNum();
+        res.p = num.p / n;
+        res.q = num.q / n;
+
+        return res;
     }
 
     public static void main(String[] args) {
@@ -23,7 +28,13 @@ public class Main {
         // Använd RatNum från upg. 13.3
         // Hitta största gemensamma delare som i upg. 6.2
 
+        RatNum num = new RatNum();
+        num.p = 5;
+        num.q = 15;
 
+        RatNum simplified = simplify(num);
+
+        System.out.format("%d/%d", simplified.p, simplified.q);
     }
 }
 
